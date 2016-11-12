9a9c9d014b657778e9640f151e691f62f11653c3
Sam Noedel <sam.noedel@gmail.com>
2016-11-12 12:35:53 -0800

    Create Dockerfile

--- src/index.js
+++ src/index.js
@@ -9,27 +9,27 @@ server.use(restify.CORS());
 
 server.get('/sets', require('./handlers/set-browse'));
 server.get('/cards', (req, res, next) => {
-  let db
+  let db;
 
   MongoClient.connect('mongodb://card-db/rune')
     .then(database => {
-      db = database
-      let collection = db.collection('cards')
-      return collection.find({ }, { name: 1, colors: 1, _id: 0 }).toArray()
+      db = database;
+      let collection = db.collection('cards');
+      return collection.find({ }, { name: 1, colors: 1, _id: 0 }).toArray();
     })
     .then(docs => {
       let cards = R.map(doc => ({
         name: doc.name,
         colors: serializeColors(doc.colors)
-      }), docs)
-      res.json(cards)
+      }), docs);
+      res.json(cards);
     })
     .catch(err => console.error(err))
     .then(() => {
-      db.close()
-      next()
-    })
-})
+      db.close();
+      next();
+    });
+});
 
 server.get('/card/:name', (req, res, next) => {
   let db;
@@ -63,13 +63,13 @@ server.get('/card/:name', (req, res, next) => {
 });
 
 server.get('/set/:code', (req, res, next) => {
-  let db
+  let db;
 
   MongoClient.connect('mongodb://card-db/rune')
     .then(database => {
-      db = database
-      let collection = db.collection('sets')
-      let code = req.params.code.toUpperCase()
+      db = database;
+      let collection = db.collection('sets');
+      let code = req.params.code.toUpperCase();
       return collection.findOne(
         { code },
         {
@@ -79,7 +79,7 @@ server.get('/set/:code', (req, res, next) => {
             'cards.name': 1,
             'cards.colors': 1
           }
-        })
+        });
     })
     .then(set => {
       if (!set) res.send(404)
@@ -87,16 +87,16 @@ server.get('/set/:code', (req, res, next) => {
         set = {
           name: set.name,
           cards: R.map(card => ({ name: card.name, colors: serializeColors(card.colors) }), set.cards)
-        }
-        res.json(set)
+        };
+        res.json(set);
       }
     })
     .catch(err => next(err))
     .then(() => {
-      db.close()
-      next()
-    })
-})
+      db.close();
+      next();
+    });
+});
 
 function serializeColors(colors) {
   if (!colors) {
@@ -115,9 +115,9 @@ function serializeColors(colors) {
       case 'Green':
         return 'g'
     }
-  }, colors).join('')
+  }, colors).join('');
 }
 
-server.listen(8080, () => {
-  console.log(`${server.name} listening at ${server.url}`)
-})
+server.listen(80, () => {
+  console.log(`${server.name} listening at ${server.url}`);
+});
